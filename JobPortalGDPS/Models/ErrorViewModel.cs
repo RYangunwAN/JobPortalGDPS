4b7ece847b4b73443198a9b1ae0d885991e78b12
RYangunawAN <ryangunawan.we@gmail.com>
2024-09-04 21:05:37 +0700

    Add project files.

---
+++ JobPortalGDPS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JobPortalGDPS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
